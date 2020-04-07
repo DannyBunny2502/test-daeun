890740dd30e32f215ac9289203b4dd6d758bfcdb
user <user@DESKTOP-K53B4PG>
2020-04-07 22:05:08 +0900

    First Commit > 2020-04-07

---
+++ test-daeun/src/main/java/com/home/uhd/TestDaeunApplication.java
@@ -0,0 +1,13 @@
+package com.home.uhd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDaeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDaeunApplication.class, args);
+	}
+
+}
